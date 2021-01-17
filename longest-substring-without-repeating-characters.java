3a03f5b7833ed52d824990bfc826100482538317
Kaavya Lakshmanan <64374421+kaavyalakshmanan@users.noreply.github.com>
2021-01-17 12:27:33 -0800

    Create longest-substring-without-repeating-characters.java

---
+++ longest-substring-without-repeating-characters.java
@@ -0,0 +1,23 @@
+// Algorithnm: Sliding window using int array to keep track to ASCII characters' indices
+// Running time: O(n) --> see every character in s exactly once
+// Additional space: O(m) where m = 128
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        int longestSubstring = 0;
+        // For ASCII --> this initializes an int array with indices going from 0 to 127, all initialized to 0
+        int[] chars = new int[128];
+        int left = 0;
+        int right = 0;
+        
+        // Left and right are either ends of current window
+        while (right < s.length()) {
+            left = Math.max(left, chars[s.charAt(right)]);
+            longestSubstring = Math.max(longestSubstring, (right-left)+1);
+            chars[s.charAt(right)] = right+1;
+            right+=1;
+        }
+        return longestSubstring;
+    }
+}
